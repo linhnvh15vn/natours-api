f6e8fbba9abbb75f1e25f2528e9662a5618f73c0
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-23 08:47:04 +0700

    connect to database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const dotenv = require("dotenv");
 const app = require("./app");
+const { default: mongoose } = require("mongoose");
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then((_) => {
+  console.log("MongoDB connection successful!");
+});
 
 const PORT = process.env.PORT;
 
