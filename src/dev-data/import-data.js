19be3872bc3dd42bdbe0c45edf55fc92749b45a5
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-26 15:04:06 +0700

    chore: add script to import dev-data

---
+++ src/dev-data/import-data.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const fs = require("fs");
+const dotenv = require("dotenv");
+const Tour = require("../models/tour.schema");
+const User = require("../models/user.schema");
+const { default: mongoose } = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then((_) => {
+  console.log("MongoDB connection successful!");
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours.json`, "utf-8")
+);
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/users.json`, "utf-8")
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    await User.create(users, {});
+    console.log("Data successfully loaded!");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    await User.deleteMany();
+    console.log("Data successfully deleted!");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
