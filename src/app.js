57ef242edfd7f2f5a8dde1ea04251638df188de1
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-26 15:15:20 +0700

    feat: update security for http

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@
 
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const { default: helmet } = require("helmet");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/app-error");
 const authRouter = require("./routes/auth.routes");
@@ -10,15 +15,29 @@ const userRouter = require("./routes/user.routes");
 
 const app = express();
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP. Please try again in an hour!",
+});
+
 // Middlewares
+app.use(helmet());
+app.use(cors());
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+app.use(xss());
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+  res.status(200).json("Ok!");
 });
 
 // Routes
