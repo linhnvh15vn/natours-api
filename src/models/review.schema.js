6fb498364c1da664c065ad0c274b5056c74ad7b2
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-29 16:35:16 +0700

    feat: calculate tour rating base on review

--- src/models/review.schema.js
+++ src/models/review.schema.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const Tour = require("./tour.schema");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -29,8 +30,6 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
-const Review = mongoose.model("Review", reviewSchema);
-
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: "user",
@@ -39,5 +38,56 @@ reviewSchema.pre(/^find/, function (next) {
 
   next();
 });
+
+reviewSchema.post("save", function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: touId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  /*   const stats = [
+    {
+      _id: "",
+      nRating: 9,
+      avgRating: 4.5,
+    },
+  ]; */
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    // No review
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
